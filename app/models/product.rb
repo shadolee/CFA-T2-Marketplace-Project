b1771626ceb9aa096003d63e7a94a7826ca09109
Lee McDowell <shadolee@mail.com>
2017-04-18 13:24:03 +1000

    add carrierwave

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  mount_uploaders :images, RoomUploader
-  # serialize :images, JSON 
+  mount_uploaders :images, ImageUploader
+
 end
