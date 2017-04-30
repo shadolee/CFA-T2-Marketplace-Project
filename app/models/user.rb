9ff29e411d67d597ff3254199a8c68bc6e742268
Lee McDowell <shadolee@mail.com>
2017-04-30 21:27:24 +1000

    deleted role

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   enum role: [:user, :editor, :admin]
 	after_initialize :set_default_role, :if => :new_record?
 
-	def set_default_role
-		self.role ||= :user
-	end
+	# def set_default_role
+	# 	self.role ||= :user
+	# end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
